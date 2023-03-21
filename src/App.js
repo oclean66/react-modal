333f13b08f24de609b2201da1d872fc807f008ee
oclean66 <oclean66@gmail.com>
2023-03-21 02:02:19 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React from 'react'
+// import logo from './logo.svg';
+// import imagen from './modal.png';
 import './App.css';
+import { Button, Header, Icon, Image, Modal } from 'semantic-ui-react'
 
 function App() {
+  const [open, setOpen] = React.useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Modal
+        basic
+        closeOnDimmerClick={false}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        // closeIcon
+        style={{ position: 'initial' }}
+        size='fullscreen'
+
+      // trigger={<Button>Basic Modal</Button>}
+      >
+        {/* <Header icon>
+        <Icon name='archive' />
+        Archive Old Messages
+      </Header> */}
+        <Modal.Content style={{ textAlign: 'center' }}>
+          <p>
+            <Image src='https://kingdeportes.net/react-modal/modal.png' wrapped />
+          </p>
+        </Modal.Content>
+        <Modal.Actions>
+          {/* <Button basic color='red' inverted >
+            <Icon name='remove' /> No
+          </Button> */}
+          <Button color='green' inverted onClick={()=>setOpen(false)}>
+            <Icon name='checkmark' /> Entendido!
+          </Button>
+        </Modal.Actions>
+      </Modal>
     </div>
   );
 }
